28c4e152cb26067f991cc69fb04d81fcf82f3376
erinlee316 <127688889+erinlee316@users.noreply.github.com>
2025-06-01 10:35:14 -0700

    Add files via upload

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    return (2 * ((1*2*3*4*5*6) + (1*2*3*4*5) + (1*2*3*4*5) + (1*2*3*4) + (1*2*3) + (1*2*3) + (1*2*3) + (1*2) + (1*2) + (1*2) + (1*2) + (1*2)))
